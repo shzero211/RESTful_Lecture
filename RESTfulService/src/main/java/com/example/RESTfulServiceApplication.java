33bb8818f397bf584fb2f9af3e7e200fb3052dda
SangHun <shzero211@naver.com>
2022-03-08 19:21:56 +0900

    [UPDATE]스프링부트 버전변경,yml파일로변경

---
+++ RESTfulService/src/main/java/com/example/RESTfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulServiceApplication.class, args);
+	}
+
+}
