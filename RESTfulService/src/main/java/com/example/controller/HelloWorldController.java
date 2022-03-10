6a541b3fc90253ee7d34b662d752a4b4cba61791
SangHun <shzero211@naver.com>
2022-03-10 17:03:46 +0900

    [ADD]HelloBean,Logging

---
+++ RESTfulService/src/main/java/com/example/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.HelloWorldBean;
+
+import lombok.Getter;
+
+@RestController
+public class HelloWorldController {
+
+	//GET
+	// /hello-world(endpoint라고 부름)
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	//alt+enter:새클래스 만들기
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	
+}
